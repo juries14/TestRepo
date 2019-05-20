24a27d8a33d219d0433c3e652c7e0e27f2e1640d
Juries <markjuriesmabiling@gmail.com>
2019-05-20 16:11:19 +0800

    initial

---
+++ GitUpload-Sample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitUpload_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sample Git Upload");
+            Console.WriteLine("Testing");
+            Console.ReadLine();
+        }
+    }
+}
